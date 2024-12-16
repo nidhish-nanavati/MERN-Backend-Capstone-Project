7cada93486ba03dfea2c2cb7ed0200291d907622
Nidhish Nanavati <nanostudmaterial@gmail.com>
2024-12-17 00:51:54 +0530

    added login/register routes and jwt tokenization

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ const app = express();
 const path = require('path');
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const userRoute = require("./routes/user");
 
 dotenv.config();
 app.get('/',(req,res)=>{
     // res.send("Hello World");
     res.sendFile(path.join(__dirname,"public","try.html")); 
 });
+
+// Define a health API check endpoint
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        status: 'UP',
+        timestamp: new Date().toISOString()
+    });
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use("/api/user",userRoute);
 
 const PORT = process.env.PORT || 3000;
 
